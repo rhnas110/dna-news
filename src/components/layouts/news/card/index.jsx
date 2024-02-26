982cd33c60381b2a4c49fa3ef48386c1245a6bc6
hanbas <51848196+rhnas110@users.noreply.github.com>
2024-02-26 19:21:05 +0700

    feat: add news page (#3)

--- src/components/layouts/news/card/index.jsx
+++ src/components/layouts/news/card/index.jsx
@@ -37,6 +37,43 @@ export const TopHeadlinesCard = ({ news }) => {
   );
 };
 
-export const Card = () => {
-  return <div>Card</div>;
+export const Card = ({ news }) => {
+  return (
+    <a href={news?.url} target="_blank">
+      <div className="flex sm:flex-row flex-col justify-between gap-x-4 bg-stone-700 rounded sm:pl-2 p-2 sm:p-0 group">
+        <div className="flex flex-1 gap-x-2 py-2">
+          <div className="w-[30%] md:block hidden">
+            <p className="opacity-80">{news?.name}</p>
+            <div className="mt-4">
+              <p className="font-bold">{news?.author}</p>
+              <p className="opacity-80 italic">
+                {beautifyDate(news?.publishedAt)}
+              </p>
+            </div>
+          </div>
+          <div className="flex-1">
+            <p className="font-bold text-lg sm:text-xl md:text-2xl text-rose-600 group-hover:underline group-hover:underline-offset-2">
+              {news?.title}
+            </p>
+            <p className="sm:block hidden">{news?.description}</p>
+          </div>
+        </div>
+
+        <div className="rounded overflow-hidden -order-1 sm:order-none flex justify-center flex-col relative">
+          <img
+            src={news?.image}
+            alt={news?.title}
+            className="aspect-square w-full sm:w-60"
+            loading="lazy"
+          />
+          <p className="opacity-80 sm:hidden">{news?.name}</p>
+          {news?.clicked && (
+            <span className="absolute top-1 right-1 p-1 rounded bg-rose-600/60 font-semibold">
+              Sudah Dibaca
+            </span>
+          )}
+        </div>
+      </div>
+    </a>
+  );
 };
--- src/components/layouts/news/index.jsx
+++ src/components/layouts/news/index.jsx
@@ -1,40 +1,37 @@
-import { useCallback, useContext, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
-import { GoMoveToTop } from "react-icons/go";
+import { useSearchParams } from "react-router-dom";
 
-import { SearchNewsContext } from "@/context/SearchNewsContext";
-
-import { Loading } from "@/components/layouts/news/loading";
 import { Card } from "@/components/layouts/news/card";
-import { Button } from "@/components/ui/button";
+import { Button, ButtonToTop } from "@/components/ui/button";
 
-import { cn, scrollTo } from "@/lib/utils";
 import useLocalStorage from "@/hooks/useLocalStorage";
+import { generateDummyData, popularSearch } from "@/lib/utils";
+
+const dummyData = generateDummyData(8);
 
 export const News = () => {
-  const { query } = useContext(SearchNewsContext);
   const [news, setNews] = useState([]);
   const [totalResults, setTotalResults] = useState(0);
   const [newsClicked, _] = useLocalStorage("news", []);
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const q = searchParams.get("q");
 
-  const url_api = query
-    ? `https://newsapi.org/v2/everything?q=${query}&apiKey=${
-        import.meta.env.VITE_NEWS_API_KEY
-      }`
-    : `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${
-        import.meta.env.VITE_NEWS_API_KEY
-      }`;
+  const url_api = `https://newsapi.org/v2/everything?q=${q}&apiKey=${
+    import.meta.env.VITE_NEWS_API_KEY
+  }`;
 
   const fetchNews = useCallback(async () => {
     try {
+      if (!q) return false;
       const response = await (await axios.get(url_api)).data;
 
       if (response.status === "ok") {
         setTotalResults(response.totalResults);
-        const result = response.articles.map((article, index) => {
+        const result = response.articles.map((article) => {
           const data = {
             id: uuidv4(),
             title: article?.title,
@@ -48,75 +45,24 @@ export const News = () => {
           if (newsClicked.includes(article.title)) {
             data.clicked = true;
           }
-          if (index === 0)
-            return {
-              ...data,
-              className:
-                "md:col-span-2 md:row-span-2 w-full h-[500px] rounded p-1 shadow-md bg-stone-700",
-              withDesc: true,
-              isFullTitle: true,
-            };
-          else if (index === 1)
-            return {
-              ...data,
-              className:
-                "md:col-start-3 w-full h-[300px] md:h-[250px] rounded p-1 shadow-md bg-stone-800",
-            };
-          else if (index === 2)
-            return {
-              ...data,
-              className:
-                "md:col-start-4 w-full h-[300px] md:h-[250px] rounded p-1 shadow-md bg-stone-800",
-            };
-          else if (index === 3)
-            return {
-              ...data,
-              className:
-                "md:col-start-3 md:row-start-2 w-full h-[300px] md:h-[250px] rounded p-1 shadow-md bg-stone-800",
-            };
-          else if (index === 4)
-            return {
-              ...data,
-              className:
-                "md:col-start-4 md:row-start-2 w-full h-[300px] md:h-[250px] rounded p-1 shadow-md bg-stone-800",
-            };
-          else if (index === 5 || index === 6)
-            return {
-              ...data,
-              className:
-                "md:row-start-3 w-full h-[300px] md:h-[250px] rounded p-1 shadow-md bg-stone-800",
-            };
-          else if (index === 7)
-            return {
-              ...data,
-              className:
-                "md:col-start-1 md:row-start-4 w-full h-[300px] md:h-[250px] rounded p-1 shadow-md bg-stone-800",
-            };
-          else if (index === 8)
-            return {
-              ...data,
-              className:
-                "md:col-span-2 md:row-span-2 md:col-start-3 md:row-start-3 w-full h-[500px] rounded p-1 shadow-md bg-stone-700",
-              withDesc: true,
-              isFullTitle: true,
-            };
-          else
-            return {
-              ...data,
-              className:
-                "w-full h-[300px] md:h-[250px] rounded p-1 shadow-md bg-stone-800",
-            };
+          return {
+            ...data,
+          };
         });
         setNews(result);
 
         // back to default
         setPage(1);
-        setHasMore(true);
+        if (response.totalResults > 100) {
+          setHasMore(true);
+        } else {
+          setHasMore(false);
+        }
       }
     } catch (error) {
       console.log(error);
     }
-  }, [url_api, newsClicked]);
+  }, [url_api, newsClicked, q]);
 
   useEffect(() => {
     fetchNews();
@@ -145,8 +91,6 @@ export const News = () => {
 
           return {
             ...data,
-            className:
-              "w-full h-[300px] md:h-[250px] rounded p-1 shadow-md bg-stone-800",
           };
         });
         setNews((prevNews) => [...prevNews, ...result]);
@@ -161,15 +105,33 @@ export const News = () => {
       console.log(error);
     }
   }, [newsClicked, url_api, page, news?.length]);
+
+  if (!q) {
+    const popular = popularSearch();
+    return (
+      <div className="text-center">
+        <h2 className="opacity-80">Your query is invalid</h2>
+        <div className="flex gap-1 justify-center">
+          <p className="opacity-80">Try search </p>
+          <span
+            className="font-semibold underline cursor-pointer"
+            onClick={() => setSearchParams(`q=${popular}`)}
+          >
+            {popular}!
+          </span>
+        </div>
+      </div>
+    );
+  }
   return (
     <>
       {news?.length ? (
         <>
-          {!!query && (
+          {!!q && (
             <div className="my-8 text-gray-300">
               <p className="text-lg md:text-4xl">
                 Search results for:{" "}
-                <span className="font-bold text-gray-200">{query}</span>
+                <span className="font-bold text-gray-200">{q}</span>
               </p>
 
               <hr className="my-4 h-0.5 border-t-0 bg-transparent bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 opacity-25 rounded" />
@@ -183,14 +145,10 @@ export const News = () => {
               </p>
             </div>
           )}
-          <div className="grid md:grid-cols-4 md:grid-rows-5 gap-4 place-items-center">
+          <div className="flex flex-col gap-y-4">
             {news?.map((news) => {
               return (
                 <div
-                  className={cn(
-                    "w-[240px] h-[360px] inline-block relative p-2",
-                    news.className
-                  )}
                   key={news?.id}
                   onClick={() => {
                     const data = [news.title];
@@ -217,17 +175,43 @@ export const News = () => {
             >
               More
             </Button>
-            <Button
-              className="w-12 h-12 absolute top-1 right-0 rounded-full bg-rose-600 hover:bg-rose-700 shadow-lg"
-              onClick={() => scrollTo(0, 0)}
-            >
-              <GoMoveToTop size={25} />
-            </Button>
+            <ButtonToTop />
           </div>
         </>
       ) : (
-        <div className="grid md:grid-cols-4 md:grid-rows-5 gap-4 animate-pulse">
-          <Loading />
+        <div className="flex flex-col gap-y-4 animate-pulse">
+          {dummyData.map(({ id }) => {
+            return (
+              <div
+                key={id}
+                className="flex sm:flex-row flex-col justify-between gap-x-4 bg-stone-700 rounded sm:pl-2 p-2 sm:p-0 w-full h-60"
+              >
+                <div className="flex flex-1 gap-x-2 py-2">
+                  <div className="w-[30%] md:block hidden">
+                    <div className="h-4 w-1/2 bg-stone-300 rounded-full"></div>
+                    <div className="mt-4 w-1/2">
+                      <div className="h-6 bg-stone-400 rounded-full"></div>
+                      <div className="h-2 bg-stone-300 rounded-full mt-2"></div>
+                    </div>
+                  </div>
+                  <div className="flex-1">
+                    <div className="h-6 bg-stone-400 rounded-full"></div>
+                    <div className="h-2 bg-stone-300 rounded-full mt-2 sm:hidden"></div>
+
+                    <div className="sm:block hidden">
+                      <div className="h-2 bg-stone-300 rounded-full mt-2"></div>
+                      <div className="h-2 bg-stone-300 rounded-full mt-2"></div>
+                      <div className="h-2 bg-stone-300 rounded-full mt-2"></div>
+                    </div>
+                  </div>
+                </div>
+                <div className="rounded overflow-hidden -order-1 sm:order-none flex justify-center flex-col">
+                  <div className="rounded aspect-square w-full sm:w-60 bg-stone-400 mx-auto"></div>
+                  <div className="h-2 w-1/3 bg-stone-300 rounded-full mt-2 opacity-80 sm:hidden"></div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       )}
     </>
