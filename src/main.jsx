6f36f69b9c49725c82fd363be5b4837f3e3a819b
hanbas <51848196+rhnas110@users.noreply.github.com>
2024-03-04 15:25:44 +0700

    refactor: delete search context (#6)

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
-// CONTEXT
-import { SearchNewsProvider } from "./context/SearchNewsContext";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
-    <SearchNewsProvider>
-      <App />
-    </SearchNewsProvider>
+    <App />
   </Router>
 );
